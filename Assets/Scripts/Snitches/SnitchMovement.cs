5cf8735d27a7f1212b247785f11046ec080ccf8e
Dzubaart <a.dzuba@atomgroup.io>
2025-01-22 21:22:46 +0300

    Правка багов

--- Assets/Scripts/Snitches/SnitchMovement.cs
+++ Assets/Scripts/Snitches/SnitchMovement.cs
@@ -1,11 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace HarryPoter.Core.Snitches
 {
     public class SnitchMovement : MonoBehaviour
     {
+        [Header("Configs")]
         [SerializeField] private float _minDistance = 0.1f;
+        [SerializeField] private float _movementSpeed = 1f;
+        [SerializeField] private float _rotationSpeed = 5f;
         [SerializeField] private List<Transform> _points;
         
         private int _currentPoint;
@@ -14,16 +18,16 @@ namespace HarryPoter.Core.Snitches
             transform.position = _points[_currentPoint].position;
         }
 
-        private void Update()
+        private void FixedUpdate()
         {
             if ((transform.position - _points[_currentPoint].position).sqrMagnitude < _minDistance)
             {
                 _currentPoint = (_currentPoint + 1) % _points.Count;
             }
-
-            transform.position = Vector3.MoveTowards(transform.position, _points[_currentPoint].position, Time.deltaTime * 0.1f);
+            
+            transform.position = Vector3.MoveTowards(transform.position, _points[_currentPoint].position, Time.fixedDeltaTime * _movementSpeed);
             transform.rotation = Quaternion.Lerp(transform.rotation,
-                Quaternion.LookRotation(_points[_currentPoint].position - transform.position), Time.deltaTime*5);
+                Quaternion.LookRotation(_points[_currentPoint].position - transform.position), Time.fixedDeltaTime*_rotationSpeed);
         }
     }
 }
