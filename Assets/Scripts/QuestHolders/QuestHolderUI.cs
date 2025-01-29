312cb9f3dbcb59da8c5bdaee9b7ce73024505da2
Dzubaart <a.dzuba@atomgroup.io>
2025-01-29 09:51:20 +0300

    Правки на сцене

--- Assets/Scripts/QuestHolders/QuestHolderUI.cs
+++ Assets/Scripts/QuestHolders/QuestHolderUI.cs
@@ -12,19 +12,16 @@ namespace HarryPoter.Core.Quests
         [Header("UIs")]
         [SerializeField] private RectTransform _questPanel;
         [SerializeField] private TMP_Text _questTitle;
-        [SerializeField] private RectTransform _completePanel;
         
         private void Update()
         {
             if (_questHolder.IsComplete || _questHolder.CurrentQuest == null)
             {
-                _completePanel.gameObject.SetActive(true);
                 _questPanel.gameObject.SetActive(false);
             }
             else
             {
                 _questTitle.text = _questHolder.CurrentQuest.QuestTitle;
-                _completePanel.gameObject.SetActive(false);
                 _questPanel.gameObject.SetActive(true);
             }
         }
